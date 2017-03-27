efa87c0c6cf197584545c25303fa9678914aa5a4
Lucas Rodrigues <lucas.rodrigues20@outlook.com>
2017-03-27 19:10:18 -0300

    fileInfo endpoint and sync request

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 'use strict';
 
 const express = require('express');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const fs = require('fs');
 const app = express();
 
 app.use(express.static('public'));
+
+app.post('/fileInfo', upload.single('archive'), (req, res) => {
+
+    if (req.file) {
+        const size  = req.file.size;
+        fs.unlinkSync(req.file.path); // deletes the file after gets its size
+        
+        return res.json({ size: req.file.size });
+    }
+
+    throw new Error("Please, submit a file");
+
+});
+
+app.use((req, res, next) => {
+    res.status(404).json({ error: '404 - Not Found' });
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).json({ error: err.message });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
     console.log(`Running at port ${listener.address().port}`);
--- test/server-test.js
+++ test/server-test.js
@@ -3,6 +3,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('./../server');
+const fs = require('fs');
 const assert = chai.assert;
 
 chai.use(chaiHttp);
@@ -18,6 +19,41 @@ describe('Endpoints', () => {
                 .end((err, res) => {
 
                     assert.strictEqual(res.status, 200);
+
+                    done();
+
+                });
+
+        });
+
+    });
+
+    describe('POST /fileInfo', () => {
+
+        it('returns a JSON with the file size when receives a file uploaded', (done) => {
+
+            chai.request(server)
+                .post('/fileInfo')
+                .attach('archive', fs.readFileSync(__dirname + '/test-file.txt'), 'test-file.txt')
+                .end((err, res) => {
+
+                    assert.strictEqual(res.status, 200);
+                    assert.strictEqual(res.body.size, 10);
+
+                    done();
+
+                })
+
+        });
+
+        it('returns an error when receives no file', (done) => {
+
+            chai.request(server)
+                .post('/fileInfo')
+                .end((err, res) => {
+
+                    assert.strictEqual(res.status, 500);
+                    assert.strictEqual(res.body.error, 'Please, submit a file');
 
                     done();
 
